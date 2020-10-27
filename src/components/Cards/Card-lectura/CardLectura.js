40cfe4f750e62c511ce5c96bb575bd8fe21bef7b
Pitunetix <griusech89@gmail.com>
2020-10-27 20:17:11 -0300

    Estilos a los Cards de Lectura
    Empiezo de a poco darle estilos a los Cards de Lectura

--- src/components/Cards/Card-lectura/CardLectura.js
+++ src/components/Cards/Card-lectura/CardLectura.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './CardLectura.css'
 import Button from '@material-ui/core/Button';
+import Hoja from '../../../img/clip-de-papel.svg'
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 
 const CardLectura = () => {
     return (
@@ -9,18 +11,34 @@ const CardLectura = () => {
                 <div className="card-header">
                     
                         <div className="item1">
-                            ITEM1
+                            <div className="item-things">
+                                <div className="item-icon">
+                                    <ChatBubbleIcon />
+                                </div>
+                                <div className="item-info">
+                                    <h4>Lectura</h4>
+                                    <p>Cuentos para aprender ortografía</p>
+                                </div>
+                            </div>
+
                         </div> 
 
-                        <div className="item6">
+                        <div className="item2">
+                            texto loco loco loco loco loco loco 
+                        </div>
+                        <div className="item3">
+                            Progreso
+                        </div>
+
+                        <div className="item4">
                             <Button>
-                            Primario
+                                Continuar
                             </Button> 
                         </div>
                     
                 </div>
                 <div className="card-footer">
-                    TEXTO 1 <span>Texto 2</span>
+                   Texto <ChatBubbleIcon />
                 </div>
             </div>
         </div>
