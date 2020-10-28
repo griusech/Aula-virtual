46e283c2d6d94a4bb634af9b8422a61e7c44c1e6
Pitunetix <griusech89@gmail.com>
2020-10-27 23:29:45 -0300

    Diseño y props
    Empiezo a pasarle props a la card y acomodar los estilos junto con los iconos

--- src/components/Lectura/Lectura.js
+++ src/components/Lectura/Lectura.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import CardLectura from '../Cards/Card-lectura/CardLectura'
+import Progress from '../Icons/Progress'
+import CalendarTodayOutlinedIcon from '@material-ui/icons/CalendarTodayOutlined';
 
 const Lectura = () => {
     return (
         <div>
-            <CardLectura />
+            <CardLectura 
+                entrega="Fecha de Entrega"
+                fecha="15 de Octubre"
+                continuar = "Continuar"
+                calendar={<CalendarTodayOutlinedIcon />}
+                progressIcon={<Progress />}
+                progress="Progreso"
+                numberProgress="25%"
+            />
+            {/* <CardLectura 
+                entrega="Fecha de Entrega"
+                fecha="15 de Octubre"
+            />
+            <CardLectura 
+                entrega="Fecha de Entrega"
+                fecha="15 de Octubre"
+            />
+            <CardLectura 
+                entrega="Fecha de Entrega"
+                fecha="15 de Octubre"
+            /> */}
         </div>
     )
 }
