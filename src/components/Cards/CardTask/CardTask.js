ac94705e7d9cceec5a17020a13e9f2540366e871
Pitunetix <griusech89@gmail.com>
2020-10-29 20:59:07 -0300

    Detalles finales
    Di los ultimos detalles de diseño

--- src/components/Cards/CardTask/CardTask.js
+++ src/components/Cards/CardTask/CardTask.js
@@ -1,98 +1,74 @@
-import React from 'react'
-import './CardTask.css'
-import Button from '@material-ui/core/Button';
-import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import React from "react";
+import "./CardTask.css";
+import Button from "@material-ui/core/Button";
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
 
 const CardLectura = (
-    {
-    fecha, 
-    entrega, 
-    textButton,
-    calendar, 
-    progressIcon, 
-    progress, 
-    iconTask,
-    newTask,
-    delayedTask,
-    numberProgress
-    }) => {
-
-    return (
-        <div className="card-container">
-            <div className="card-section">
-                <div className="card-header">
-                    
-                        <div className="item-iconTask">
-                            {iconTask}
-                        </div>
-                        <div className="item1">
-                            <div className="item-things">
-                                <div className="item-infoTask">
-                                    <h5>LECTURA</h5>
-                                    <p>Cuentos para aprender ortografía</p>
-                                    <p>{newTask ? newTask : delayedTask}</p>
-                                </div>
-                            </div>
-
-                        </div> 
-
-                        <div className="item2">
-                            <div className="item-things">
-                                <div className="item-icon">
-                                    {calendar}
-
-                                </div>
-                                <div className="item-info">
-                                    <h5>{entrega}</h5>
-                                    <p className={delayedTask ? "conClase" : ""}>{fecha}</p>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div className="item3">
-                            <div className="item-things">
-                                <div className="item-icon">
-                                    {progressIcon}
-
-                                </div>
-                                <div className="item-info">
-                                    {
-                                        
-                                    }
-                                    <h5>{progress}</h5>
-                                    <p>{numberProgress}</p>
-                                </div>
-
-                            </div>
-                        </div>
-
-                       
-
-                        <div className={textButton ? "item4" : "item4 v-hidden"}>
-                            <Button>
-                                {textButton}
-                            </Button> 
-                        </div>
-                        
-                    
-                </div>
-                <div className="card-footer">
-                   <div className="boton-materia">
-                    <div className="circle">
-
-                    </div>
-                    <div className="materia">
-                    Lengua
-
-                    </div>
-                   </div>
-                   <div className="chatIcon">
-                        <ChatBubbleIcon />
-                   </div>
-                </div>
+ {
+  title,
+  description,
+  date,
+  delivery,
+  textButton,
+  calendar,
+  progressIcon,
+  progress,
+  iconTask,
+  newTask,
+  delayedTask,
+  numberProgress,
+  matter
+}) => {
+  return (
+    <div className="card-container">
+      <div className="card-section">
+        <div className="card-header">
+          <div className="item-iconTask">{iconTask}</div>
+          <div className="item1">
+            <div className="item-data">
+              <div className="item-infoTask">
+                <h5>{title}</h5>
+                <p>{description}</p>
+                <p>{newTask ? newTask : delayedTask}</p>
+              </div>
             </div>
-        </div>
-    )
-}
+          </div>
 
-export default CardLectura
+          <div className="item2">
+            <div className="item-data">
+              <div className="item-icon">{calendar}</div>
+              <div className="item-info">
+                <h5>{delivery}</h5>
+                <p className={delayedTask ? "conClase" : ""}>{date}</p>
+              </div>
+            </div>
+          </div>
+          <div className="item3">
+            <div className="item-data">
+              <div className="item-icon">{progressIcon}</div>
+              <div className="item-info">
+                <h5>{progress}</h5>
+                <p>{numberProgress}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={textButton ? "item4" : "item4 v-hidden"}>
+            <Button>{textButton}</Button>
+          </div>
+        </div>
+        <div className="card-footer">
+          <div className="boton-materia">
+            <div className="circle"></div>
+            <div className="materia">{matter}</div>
+          </div>
+          <div className="chatIcon">
+            <ChatBubbleIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardLectura;
--- src/components/TasksList/TasksList.js
+++ src/components/TasksList/TasksList.js
@@ -1,68 +1,144 @@
-import React from 'react'
-import CardTask from '../Cards/CardTask/CardTask'
-import Progress from '../Icons/Progress'
-import Task from '../Icons/Taks'
-import NewTask from '../Icons/NewTask'
-import NoProgress from '../Icons/NoProgress'
-import Completed from '../Icons/Completed'
-import Delayed from '../Icons/Delayed'
-import CalendarTodayOutlinedIcon from '@material-ui/icons/CalendarTodayOutlined';
-import EventAvailableOutlinedIcon from '@material-ui/icons/EventAvailableOutlined';
+import React from "react";
+import CardTask from "../Cards/CardTask/CardTask";
+import Progress from "../Icons/Progress";
+import Task from "../Icons/Taks";
+import Pencil from "../Icons/Pencil";
+import NewTask from "../Icons/NewTask";
+import NoProgress from "../Icons/NoProgress";
+import Completed from "../Icons/Completed";
+import Delayed from "../Icons/Delayed";
+import Teacher from "../Icons/Teacher";
+import CalendarTodayOutlinedIcon from "@material-ui/icons/CalendarTodayOutlined";
+import EventAvailableOutlinedIcon from "@material-ui/icons/EventAvailableOutlined";
 
 const Lectura = () => {
-    return (
-        <div>
-            <CardTask 
-                entrega="Fecha de Entrega"
-                fecha="15 de Abril"
-                textButton = "Continuar"
-                calendar={<CalendarTodayOutlinedIcon />}
-                progressIcon={<Progress />}
-                iconTask={<Task />}
-                progress="Progreso"
-                numberProgress="25%"
-            />
-            <CardTask 
-                entrega="Fecha de Entrega"
-                fecha="15 de Abril"
-                textButton = "Comenzar"
-                calendar={<CalendarTodayOutlinedIcon />}
-                progressIcon={<NoProgress />}
-                iconTask={<Task />}
-                newTask = {<NewTask />}
-                progress="Progreso"
-                numberProgress="-"
-            />
-            <CardTask 
-                entrega="Fecha de Entrega"
-                fecha="15 de Abril"
-                textButton = "Continuar"
-                calendar={<CalendarTodayOutlinedIcon />}
-                progressIcon={<Progress />}
-                iconTask={<Task />}
-                delayedTask = {<Delayed />}
-                progress="Progreso"
-                numberProgress="25%"
-            />
-            <CardTask 
-                entrega="Fecha de Entrega"
-                fecha="15 de Abril"
-                calendar={<EventAvailableOutlinedIcon />}
-                progressIcon={<Completed />}
-                iconTask={<Task />}
-                progress="Actividad Completada"
-            />
+  return (
+    <div>
+      <CardTask
+        title="LECTURA"
+        description="Cuentos para aprender ortografía"
+        delivery="Fecha de Entrega"
+        date="15 de Abril"
+        textButton="Continuar"
+        calendar={<CalendarTodayOutlinedIcon />}
+        progressIcon={<Progress />}
+        iconTask={<Task />}
+        progress="Progreso"
+        numberProgress="25%"
+        matter="Lengua"
+      />
+      <CardTask
+        title="LECTURA"
+        description="Cuentos para aprender ortografía"
+        delivery="Fecha de Entrega"
+        date="15 de Abril"
+        textButton="Comenzar"
+        calendar={<CalendarTodayOutlinedIcon />}
+        progressIcon={<NoProgress />}
+        iconTask={<Task />}
+        newTask={<NewTask />}
+        progress="Progreso"
+        numberProgress="-"
+        matter="Lengua"
+      />
+      <CardTask
+        title="LECTURA"
+        description="Cuentos para aprender ortografía"
+        delivery="Fecha de Entrega"
+        date="15 de Abril"
+        textButton="Continuar"
+        calendar={<CalendarTodayOutlinedIcon />}
+        progressIcon={<Progress />}
+        iconTask={<Task />}
+        delayedTask={<Delayed />}
+        progress="Progreso"
+        numberProgress="25%"
+        matter="Lengua"
+      />
+      <CardTask
+        title="LECTURA"
+        description="Cuentos para aprender ortografía"
+        delivery="Fecha de Entrega"
+        date="15 de Abril"
+        calendar={<EventAvailableOutlinedIcon />}
+        progressIcon={<Completed />}
+        iconTask={<Task />}
+        numberProgress="Actividad Completada"
+        matter="Lengua"
+      />
 
-            {/* <CardTask 
-                entrega="Fecha de Entrega"
-                fecha="15 de Abril"
-                calendar={<EventAvailableOutlinedIcon />}
-                progressIcon={<Completed />}
-                iconTask={<Task />}
-                progress="Actividad Completada"
-            /> */}
-        </div>
-    )
-}
+      <br />
+      <hr />
 
-export default Lectura
+      <CardTask
+        title="ACTIVIDAD CON PREGUNTAS"
+        description="¿Qué es la potencia y la raiz cuadrada un número?"
+        delivery="Fecha de Entrega"
+        date="15 de Abril"
+        textButton="Continuar"
+        calendar={<CalendarTodayOutlinedIcon />}
+        progressIcon={<Progress />}
+        iconTask={<Pencil />}
+        progress="Progreso"
+        numberProgress="25%"
+        matter="Lengua"
+      />
+
+      <CardTask
+        title="ACTIVIDAD CON PREGUNTAS"
+        description="¿Qué es la potencia y la raiz cuadrada un número?"
+        delivery="Fecha de Entrega"
+        date="15 de Abril"
+        textButton="Comenzar"
+        calendar={<CalendarTodayOutlinedIcon />}
+        progressIcon={<NoProgress />}
+        iconTask={<Pencil />}
+        newTask={<NewTask />}
+        progress="Progreso"
+        numberProgress="-"
+        matter="Lengua"
+      />
+
+      <CardTask
+        title="ACTIVIDAD CON PREGUNTAS"
+        description="¿Qué es la potencia y la raiz cuadrada un número?"
+        delivery="Fecha de Entrega"
+        date="15 de Abril"
+        textButton="Continuar"
+        calendar={<CalendarTodayOutlinedIcon />}
+        progressIcon={<Progress />}
+        iconTask={<Pencil />}
+        delayedTask={<Delayed />}
+        progress="Progreso"
+        numberProgress="25%"
+        matter="Lengua"
+      />
+
+      <CardTask
+        title="ACTIVIDAD CON PREGUNTAS"
+        description="¿Qué es la potencia y la raiz cuadrada un número?"
+        delivery="Fecha de Entrega"
+        date="15 de Abril"
+        calendar={<EventAvailableOutlinedIcon />}
+        progressIcon={<Teacher />}
+        iconTask={<Pencil />}
+        numberProgress="Esperando Corrección"
+        matter="Lengua"
+      />
+
+      <CardTask
+        title="ACTIVIDAD CON PREGUNTAS"
+        description="¿Qué es la potencia y la raiz cuadrada un número?"
+        delivery="Fecha de Entrega"
+        date="15 de Abril"
+        calendar={<EventAvailableOutlinedIcon />}
+        progressIcon={<Completed />}
+        iconTask={<Pencil />}
+        numberProgress="Actividad Completada"
+        matter="Lengua"
+      />
+    </div>
+  );
+};
+
+export default Lectura;
